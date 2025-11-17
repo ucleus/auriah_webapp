82733537e028a5588a5ca9993cedcde6364ebe42
ucleus <ucleus444@gmail.com>
2025-11-17 07:02:38 -0500

    fixed issues with loging in to admin

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
         $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
